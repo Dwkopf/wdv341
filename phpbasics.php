d6bdad8d0e8ccaa172125dd07c5bf7658371cc6d
David Kopf <davidwkopf@gmail.com>
2020-09-09 16:54:50 -0500

    first php assignment

---
+++ phpbasics.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+
+<?php  
+    $yourName = "David Kopf";
+    echo "<h1>PHP Basics</h1>";
+    $number1 = 3;
+    $number2= 5;
+    $total=$number1+$number2;
+    echo "$number1 + $number2 = $total";
+
+
+?>
+
+</head>
+
+<body>
+
+<h2>This is my h2 element by <?php echo "$yourName" ?></h2>
+
+
+
+</body>
+</html>
